beae82f000f8f00ac2b4f6f6bf2164b7ff43bf77
BpstPc_Optiplex7020 <ankitsahay1990@gmail.com>
2024-08-30 16:12:08 +0530

    added few models

---
+++ Bpst.API/DbModels/AccounDetails.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Bpst.API.DbModels
+{
+    public class AccountDetails
+    {
+        [Key]
+        [Display(Name = "Unique ID")]
+        public int UniqueId { get; set; }
+        public string BankName { get; set; } = string.Empty;
+        public string BranchName { get; set; } = string.Empty;
+        public string AccountNo { get; set; } = string.Empty;
+        public string IFSCCode { get; set; } = string.Empty;
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User? User { get; set; }
+
+
+    }
+}
---
+++ Bpst.API/DbModels/Address.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Bpst.API.DbModels
+{
+    public class Address
+    {
+        [Key]
+        [Display(Name = "Unique ID")]
+        public int UniqueId { get; set; }
+        public string? AddressLine1 {  get; set; }
+        public string? AddressLine2 { get; set; }
+        public string? AddressLine3 { get; set; }
+        public string? City {  get; set; }
+        public string? State {  get; set; }
+        public string? Country {  get; set; }
+        public string? Pincode {  get; set; }
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User? User { get; set; }
+    }
+}
